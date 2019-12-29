1400d37980635946ea83b30f371836364a7426ce
Alerik vonSheba <justalerik@gmail.com>
2019-12-28 23:10:15 -0500

    Revert "Initial commit"
    This reverts commit 2bcc8831f20e48264a2faad866311b292b5b2237.

--- newuser.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'database.php';
-$uuid = gen_uuid();
-$stm = $pdo->prepare('INSERT INTO users (id) VALUES (:id)');
-$stm->execute(array('id' => $uuid));
-echo(json_encode(array('id_user' => $uuid)));
-?>
